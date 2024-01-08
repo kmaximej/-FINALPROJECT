42e6a69f9c1392e42beab6ce2161c1a534c53102
kmaximej <151595455+kmaximej@users.noreply.github.com>
2024-01-08 06:10:46 -0800

    Add files via upload
    Updated code

---
+++ Product.java
@@ -0,0 +1,14 @@
+package departmentstore;
+
+class Product {
+
+	    String name;
+	    double price;
+	    int stock;
+
+	    Product(String name, double price, int stock) {
+	        this.name = name;
+	        this.price = price;
+	        this.stock = stock;
+	    }
+	}
